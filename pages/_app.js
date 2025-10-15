c4f334aafe6f5f07bf991141348252c04552181a
Cedric <cedricctf11a@gmail.com>
2025-10-15 19:41:17 +0800

    added transaction popup and notification provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import { NotificationProvider, TransactionPopupProvider } from "@blockscout/app-sdk";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <NotificationProvider>
+      <TransactionPopupProvider>
+        <Component {...pageProps} />
+      </TransactionPopupProvider>
+    </NotificationProvider>
+  );
 }
+
+
