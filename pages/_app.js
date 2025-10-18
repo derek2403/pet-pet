0f7556cc1199c07a332718ef8f28b2e1ee894286
Cedric <cedricctf11a@gmail.com>
2025-10-19 02:20:23 +0800

    changed rpc url and added explorer

--- pages/_app.js
+++ pages/_app.js
@@ -6,10 +6,21 @@ import { WagmiProvider } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum, base, baseSepolia } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+const petPetTestnet = {
+  id: 2403,
+  name: 'PetPet Testnet',
+  nativeCurrency: { name: 'PetPet', symbol: 'PETPET', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://c2e90a7139bb5f5fe1c6deab725ee1a45631b952-8545.dstack-prod5.phala.network/'] },
+    public: { http: ['https://c2e90a7139bb5f5fe1c6deab725ee1a45631b952-8545.dstack-prod5.phala.network/'] }
+  },
+  testnet: true
+};
+
 const config = getDefaultConfig({
   appName: 'PetPet App',
   projectId: 'YOUR_PROJECT_ID', // Get your project ID at https://cloud.walletconnect.com
-  chains: [mainnet, polygon, optimism, arbitrum, base, baseSepolia],
+  chains: [mainnet, polygon, optimism, arbitrum, base, baseSepolia, petPetTestnet],
   ssr: true, // Enable server-side rendering for Next.js
 });
 
