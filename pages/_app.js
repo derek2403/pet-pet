8dcb9e558f9956bef531db7cc0a810eeb448e301
“Marcus” <115784464+Marcussy34@users.noreply.github.com>
2025-10-24 09:30:28 +0800

    updated font family across entire app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-geist-sans",
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={poppins.variable}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
