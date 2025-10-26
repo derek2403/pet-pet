8908957154d7d7dd8c93bf33d75957241ec100f7
JingYuan <jingyuan0926@gmail.com>
2025-10-26 22:01:53 +0800

    Update ignition module

--- smartcontract/ignition/modules/PetTemplate.ts
+++ smartcontract/ignition/modules/PetTemplate.ts
@@ -3,7 +3,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 export default buildModule("PetTemplateModule", (m) => {
   const ownerAddress = m.getParameter("ownerAddress", m.getAccount(0));
   
-  const petTemplate = m.contract("Test1", [ownerAddress]);
+  const petTemplate = m.contract("PET_NAME", [ownerAddress]);
   
   return { petTemplate };
 });
